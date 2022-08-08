0c4bd962bd1745ff626c0f8cdf3b468baf850be5
erwanlfrt <erwan.leforestier@orange.com>
2022-08-08 16:35:52 +0200

    new build

---
+++ dist/angular/focusSection.directive.d.ts
@@ -0,0 +1,8 @@
+import { ElementRef } from '@angular/core';
+import { SectionDirective } from '../types/SectionDirective';
+export declare class FocusSectionDirective {
+    private el;
+    focusSection: SectionDirective;
+    private assignConfig;
+    constructor(el: ElementRef);
+}
