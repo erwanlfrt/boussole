fc7656c84624dd08026a6c8445e7ea6fc88d085c
erwanlfrt <erwanlfrt@gmail.com>
2022-08-10 21:32:26 +0200

    test new entrypoint

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,4 +1,7 @@
 import vueSpatialNavigation from './vue/VueSpatialNavigation';
 import { SpatialNavigation } from './SpatialNavigation';
 import { Configuration, defaultConfiguration } from './types/Configuration';
-export { vueSpatialNavigation as vjsn, SpatialNavigation, Configuration, defaultConfiguration };
+import { FocusDirective } from './angular/src/lib/directives/focus.directive';
+import { FocusSectionDirective } from './angular/src/lib/directives/focusSection.directive';
+import { BoussoleModule } from './angular/src/lib/boussole.module';
+export { vueSpatialNavigation as vjsn, SpatialNavigation, Configuration, defaultConfiguration, FocusDirective, FocusSectionDirective, BoussoleModule };
